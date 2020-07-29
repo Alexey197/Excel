2154d98a04b26ef6ed9e6171ea08975545febea3
Alexey197 <margadonnn3@gmail.com>
2020-07-29 09:21:36 +0300

    errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+// const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
@@ -24,14 +24,26 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'index.html'
     }),
-    new CopyPlugin([
-      {
-        from: path.resolve(__dirname, 'src/favicon.ico'),
-        to: path.resolve(__dirname, 'dist')
-      }
-    ]),
+    // new CopyPlugin([
+    //   {
+    //     from: path.resolve(__dirname, 'src/favicon.ico'),
+    //     to: path.resolve(__dirname, 'dist')
+    //   }
+    // ]),
     new MiniCssExtractPlugin({
       filename: 'bundle.[hash].css'
     })
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
 };
